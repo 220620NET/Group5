9a919de1009d3064f5f2122cc14fefd49f542aa9
David <dave22011@yahoo.com>
2022-06-22 10:24:44 -0400

    The code I added, gets a number from the user as a stopping point

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("This is group 5's editable program");
+Console.WriteLine("What would you like to be your ending number?");
+int endingNumber = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i <= endingNumber; i++)
+{
+    Console.WriteLine(i);	
+}
+
